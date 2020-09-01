cb81918d2b9f5f62333560ff5f7d0f181ee9ca6d
moffire <alexeydudarev.spb@gmail.com>
2020-09-01 18:14:25 +0300

    removed extra fields

--- orders/admin.py
+++ orders/admin.py
@@ -4,6 +4,7 @@ from .models import Order, OrderItem
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+    extra = 0
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
