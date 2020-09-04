c777c7f79828eb8bf95292a92a51ba4a3f386c54
moffire <alexeydudarev.spb@gmail.com>
2020-09-04 17:10:53 +0300

    routes for coupon app

--- myshop/urls.py
+++ myshop/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('payment/', include('payment.urls', namespace='payment')),
+    path('coupons/', include('coupons.urls', namespace='coupons')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
